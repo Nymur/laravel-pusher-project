4b328119affa60fc9f472e321febd8f8e36ee45e
Nymur <nymurrahman03@gmail.com>
2020-02-09 00:40:55 +0600

    Like and Dislike system for reply

--- routes/api.php
+++ routes/api.php
@@ -3,3 +3,5 @@
 Route::apiResource('/questions', 'QuestionController');
 Route::apiResource('/categories', 'CategoryController');
 Route::apiResource('/questions/{question}/reply', 'ReplyController');
+Route::post('/like/{reply}', 'LikeController@likeIt');
+Route::delete('/like/{reply}', 'LikeController@unLikeIt');
