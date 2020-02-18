5b19bc6218e838d68ec1c4041fee3f8009b48cf4
Nymur <nymurrahman03@gmail.com>
2020-02-18 19:28:15 +0600

    Vuetify , Vue-router and initial Components

---
+++ resources/js/components/appHome.vue
@@ -0,0 +1,16 @@
+<template>
+ <div>
+	<toolbar />
+ 	
+	<Footer />
+ </div>
+</template>
+<script>
+import toolbar from './toolbar'
+import Footer from './footer'
+    export default {
+        components:{
+        	toolbar,Footer
+        }
+    }
+</script>
